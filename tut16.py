9b27ec4f712c408a2147f244ea1609316a6553d5
Shrey Soral <78461926+ShreySoral@users.noreply.github.com>
2022-10-19 04:23:04 -0400

    Add files via upload

---
+++ tut16.py
@@ -0,0 +1,22 @@
+# scroll bar in Tkinter
+from tkinter import *
+from tkinter import messagebox
+root = Tk()
+root.geometry("455x233")
+root.title("Scrollbar tutorial")
+# for connecting scrollbar to a Widget
+# 1. widget(yscrollcommand=scrollbar.set)
+# 2. scrollbar.config(command=widget.yview)
+
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT,fill=Y)
+# listbox=Listbox(root,yscrollcommand=scrollbar.set)
+# for i in range(344):
+#     listbox.insert(END, f"Item {i}")
+
+# listbox.pack(fill=BOTH,padx=22)
+text=Text(root,yscrollcommand=scrollbar.set)
+text.pack(fill=BOTH)
+# scrollbar.config(command=listbox.yview)
+scrollbar.config(command=text.yview)
+root.mainloop()
